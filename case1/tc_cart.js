a4ababc0390fed7f19e0ae1c7302b6a40cf1b0c3
Nicole Garay <ngaray157@west-mec.org>
2020-02-04 13:10:15 -0700

    finished case 1

--- case1/tc_cart.js
+++ case1/tc_cart.js
@@ -14,9 +14,18 @@
 var orderTotal = 0;
 var cartHTML = "<table> <tr> <th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th> </tr>";
 
-for(var i = 0; i < item.length; i++){
-   cartHTML += "<tr> <td><img src='tc_item.png' alt='" + item[i] +  "' /></td>";
-   cartHTML += "<td>" + itemDescriptions[i]; + "</td>";
-   cartHTML += "<td>" + itemPrice[i] + "</td>";
+for (var i = 0; i < item.length; i++) {
+   cartHTML += "<tr> <td><img src='tc_" + item[i] + ".png' alt='" + item[i] + "' /></td>";
+   cartHTML += "<td>" + itemDescription[i]; + "</td>";
+   cartHTML += "<td>$" + itemPrice[i] + "</td>";
    cartHTML += "<td>" + itemQty[i] + "</td>";
+   var itemCost = itemPrice[i] * itemQty[i];
+   cartHTML += "<td>$" + itemCost + "</td></tr>";
+   orderTotal += itemCost;
+   // console.log({"itemCost": itemCost, "orderTotal": orderTotal}); 
 }
+cartHTML += "<tr>";
+cartHTML += "<td colspan='4'>Subtotal</td>"
+cartHTML += "<td>$" + orderTotal + "</td></tr>";
+cartHTML += "</table>";
+document.getElementById("cart").innerHTML = cartHTML;
