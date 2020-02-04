e1d5d158a16f4e78333f463e29d19484d8007c36
Nicole Garay <ngaray157@west-mec.org>
2020-02-04 15:25:46 -0700

    finished case 2
    i finished case 2, but i am trying to debug and figure out one more problem i have with the site.

---
+++ case2/hg_report.js
@@ -0,0 +1,50 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 2
+
+   Author: Nicole Garay
+   Date:   2/4/20
+   
+   Filename: hg_report.js
+	
+*/
+var gameReport = "<h1>" + itemTitle + "</h1>";
+gameReport += "<h2>By:" + itemManufacturer + "</h2>";
+gameReport += "<img src='hg_" + itemID + ".png' alt='" + itemID + "' id='gameImg'/>";
+gameReport += "<table>";
+gameReport += "<tr><th>Product ID</th><td>" + itemID + "</td></tr>";
+gameReport += "<tr><th>List Price</th><td>" + itemPrice + "</td></tr>";
+gameReport += "<tr><th>Platform</th><td>" + itemPlatform + "</td></tr>";
+gameReport += "<tr><th>ESRB Rating</th><td>" + itemESRB + "</td></tr>";
+gameReport += "<tr><th>Condition</th><td>" + itemCondition + "</td></tr>";
+gameReport += "<tr><th>Release</th><td>" + itemRelease + "</td></tr>";
+gameReport += "</table>" + itemSummary;
+
+//displays the gameReports in the article element of the html
+document.getElementsByTagName("article")[0].innerHTML = gameReport;
+var ratingsSum = 0;
+var ratingsCount = ratings.length;
+for (var i = 0; i < ratings.length; i++) {
+   ratingsSum += ratings[i];
+}
+
+var ratingsAvg = ratingsSum / ratingsCount;
+var ratingReport = "<h1>Customer Review</h1>";
+ratingReport += "<h2>" + ratingsAvg + "out of 5 stars (" + ratingsCount + " reviews)</h2>";
+
+for (var i = 0; i < 2; i ++) {
+   ratingReport = "<div class='review'>";
+   ratingReport += "<h1>" + ratingTitles[i] + "</h1>";
+   ratingReport += "<table>";
+   ratingReport += "<tr><th>By</th><td>" + ratingAuthors[i] + "</td></tr>";
+   ratingReport += "<tr><th>Review Date<th></th><td>" + ratingDates[i] + "</td></tr>";
+   ratingReport += "<tr><th>Rating</th></td>";
+   for (var i = 0; i < ratings[i]; i++) {   
+      ratingReport = "<img src='hg_star.png'/>";
+   }
+   ratingReport += "</td></tr></table>" + ratingSummaries[i] + "</div>";
+}
+document.getElementsByTagName("aside")[0].innerHTML = ratingReport;
