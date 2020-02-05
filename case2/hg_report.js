f63190319dee21b0fb09be06ee650890a1e13453
Nicole Garay <ngaray157@west-mec.org>
2020-02-05 13:23:01 -0700

    Completed case 2
    I fixed all of my bugs and am now starting case 3.

--- case2/hg_report.js
+++ case2/hg_report.js
@@ -11,6 +11,7 @@
    Filename: hg_report.js
 	
 */
+//all of this entails putting item information from the array depending on the product chosen
 var gameReport = "<h1>" + itemTitle + "</h1>";
 gameReport += "<h2>By:" + itemManufacturer + "</h2>";
 gameReport += "<img src='hg_" + itemID + ".png' alt='" + itemID + "' id='gameImg'/>";
@@ -31,20 +32,26 @@ for (var i = 0; i < ratings.length; i++) {
    ratingsSum += ratings[i];
 }
 
+//calculates the rating average
 var ratingsAvg = ratingsSum / ratingsCount;
 var ratingReport = "<h1>Customer Review</h1>";
-ratingReport += "<h2>" + ratingsAvg + "out of 5 stars (" + ratingsCount + " reviews)</h2>";
+//adds the rating under the h1 Customer Review header
+ratingReport += "<h2>" + ratingsAvg + " out of 5 stars (" + ratingsCount + " reviews)</h2>";
 
-for (var i = 0; i < 2; i ++) {
-   ratingReport = "<div class='review'>";
+//inserts 3 newest reviews into the html page
+for (var i = 0; i < 3; i++) {
+   ratingReport += "<div class='review'>";
    ratingReport += "<h1>" + ratingTitles[i] + "</h1>";
    ratingReport += "<table>";
    ratingReport += "<tr><th>By</th><td>" + ratingAuthors[i] + "</td></tr>";
-   ratingReport += "<tr><th>Review Date<th></th><td>" + ratingDates[i] + "</td></tr>";
-   ratingReport += "<tr><th>Rating</th></td>";
-   for (var i = 0; i < ratings[i]; i++) {   
-      ratingReport = "<img src='hg_star.png'/>";
+   ratingReport += "<tr><th>Review Date</th><td>" + ratingDates[i] + "</td></tr>";
+   ratingReport += "<tr><th>Rating</th><td>";
+   for (var j = 0; j < ratings[i]; j++) {   
+      ratingReport += "<img src='hg_star.png'/>";
    }
    ratingReport += "</td></tr></table>" + ratingSummaries[i] + "</div>";
+   
+
+   //puts all the information into the aside element on the html, that way they are on the right.
+   document.getElementsByTagName("aside")[0].innerHTML = ratingReport;
 }
-document.getElementsByTagName("aside")[0].innerHTML = ratingReport;
